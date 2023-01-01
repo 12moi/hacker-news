1fe0a5e196b20c686ade3fb203c4d3cac6d03bc8
devmoi <moishadrack.com>
2023-01-01 11:51:57 +0300

    add changes

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -6,7 +6,7 @@ const SearchForm = () => {
 
   return (
     <form className='search-form' onSubmit={(e) => e.preventDefault()}>
-      <h2>search hacker news</h2>
+      <h2>Search hacker news</h2>
       <input
         type='text'
         className='form-input'
